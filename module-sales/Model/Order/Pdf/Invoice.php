d3089e6ea20aec54ad9bd200fac7a9c69f51e6df
Christian Kramer <ck.daill@gmail.com>
2021-02-15 21:05:25 +0100

    changed extends

--- module-sales/Model/Order/Pdf/Invoice.php
+++ module-sales/Model/Order/Pdf/Invoice.php
@@ -7,7 +7,7 @@ namespace Schraubermarkt\Sales\Model\Order\Pdf;
 
 use Magento\Sales\Model\ResourceModel\Order\Invoice\Collection;
 use Magento\Sales\Model\Order\Pdf\Config;
-
+use Magento\Sales\Model\Order\Pdf\Invoice as CoreInvoice;
 
 /**
  * Sales Order Invoice PDF model
@@ -15,181 +15,51 @@ use Magento\Sales\Model\Order\Pdf\Config;
  */
 class Invoice extends \Magento\Sales\Model\Order\Pdf\Invoice
 {
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
-
-    /**
-     * @var \Magento\Store\Model\App\Emulation
-     */
-    private $appEmulation;
-
-    /**
-     * @param \Magento\Payment\Helper\Data $paymentData
-     * @param \Magento\Framework\Stdlib\StringUtils $string
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Framework\Filesystem $filesystem
-     * @param Config $pdfConfig
-     * @param \Magento\Sales\Model\Order\Pdf\Total\Factory $pdfTotalFactory
-     * @param \Magento\Sales\Model\Order\Pdf\ItemsFactory $pdfItemsFactory
-     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
-     * @param \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation
-     * @param \Magento\Sales\Model\Order\Address\Renderer $addressRenderer
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Store\Model\App\Emulation $appEmulation
-     * @param array $data
+       /**
+     * Set font as regular
      *
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     * @param \Zend_Pdf_Page $object
+     * @param int $size
+     * @return \Zend_Pdf_Resource_Font
      */
-    public function __construct(
-        \Magento\Payment\Helper\Data $paymentData,
-        \Magento\Framework\Stdlib\StringUtils $string,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Framework\Filesystem $filesystem,
-        Config $pdfConfig,
-        \Magento\Sales\Model\Order\Pdf\Total\Factory $pdfTotalFactory,
-        \Magento\Sales\Model\Order\Pdf\ItemsFactory $pdfItemsFactory,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
-        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
-        \Magento\Sales\Model\Order\Address\Renderer $addressRenderer,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Store\Model\App\Emulation $appEmulation,
-        array $data = []
-    ) {
-        $this->_storeManager = $storeManager;
-        $this->appEmulation = $appEmulation;
-        parent::__construct(
-            $paymentData,
-            $string,
-            $scopeConfig,
-            $filesystem,
-            $pdfConfig,
-            $pdfTotalFactory,
-            $pdfItemsFactory,
-            $localeDate,
-            $inlineTranslation,
-            $addressRenderer,
-            $data
+    protected function _setFontRegular($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/dejavu-sans/DejaVuSansCondensed.ttf')
         );
+        $object->setFont($font, $size);
+        return $font;
     }
 
     /**
-     * Draw header for item table
+     * Set font as bold
      *
-     * @param \Zend_Pdf_Page $page
-     * @return void
+     * @param \Zend_Pdf_Page $object
+     * @param int $size
+     * @return \Zend_Pdf_Resource_Font
      */
-    protected function _drawHeader(\Zend_Pdf_Page $page)
+    protected function _setFontBold($object, $size = 7)
     {
-        /* Add table head */
-        $this->_setFontRegular($page, 10);
-        $page->setFillColor(new \Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
-        $page->setLineColor(new \Zend_Pdf_Color_GrayScale(0.5));
-        $page->setLineWidth(0.5);
-        $page->drawRectangle(25, $this->y, 570, $this->y - 15);
-        $this->y -= 10;
-        $page->setFillColor(new \Zend_Pdf_Color_Rgb(0, 0, 0));
-
-        //columns headers
-        $lines[0][] = ['text' => __('Products'), 'feed' => 35];
-
-        $lines[0][] = ['text' => __('SKU'), 'feed' => 290, 'align' => 'right'];
-
-        $lines[0][] = ['text' => __('Qty'), 'feed' => 435, 'align' => 'right'];
-
-        $lines[0][] = ['text' => __('Price'), 'feed' => 360, 'align' => 'right'];
-
-        $lines[0][] = ['text' => __('Tax'), 'feed' => 495, 'align' => 'right'];
-
-        $lines[0][] = ['text' => __('Subtotal'), 'feed' => 565, 'align' => 'right'];
-
-        $lineBlock = ['lines' => $lines, 'height' => 5];
-
-        $this->drawLineBlocks($page, [$lineBlock], ['table_header' => true]);
-        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
-        $this->y -= 20;
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/dejavu-sans/DejaVuSansCondensed.ttf')
+        );
+        $object->setFont($font, $size);
+        return $font;
     }
 
     /**
-     * Return PDF document
+     * Set font as italic
      *
-     * @param array|Collection $invoices
-     * @return \Zend_Pdf
+     * @param \Zend_Pdf_Page $object
+     * @param int $size
+     * @return \Zend_Pdf_Resource_Font
      */
-    public function getPdf($invoices = [])
+    protected function _setFontItalic($object, $size = 7)
     {
-        $this->_beforeGetPdf();
-        $this->_initRenderer('invoice');
-
-        $pdf = new \Zend_Pdf();
-        $this->_setPdf($pdf);
-        $style = new \Zend_Pdf_Style();
-        $this->_setFontBold($style, 10);
-
-        foreach ($invoices as $invoice) {
-            if ($invoice->getStoreId()) {
-                $this->appEmulation->startEnvironmentEmulation(
-                    $invoice->getStoreId(),
-                    \Magento\Framework\App\Area::AREA_FRONTEND,
-                    true
-                );
-                $this->_storeManager->setCurrentStore($invoice->getStoreId());
-            }
-            $page = $this->newPage();
-            $order = $invoice->getOrder();
-            /* Add image */
-            $this->insertLogo($page, $invoice->getStore());
-            /* Add address */
-            $this->insertAddress($page, $invoice->getStore());
-            /* Add head */
-            $this->insertOrder(
-                $page,
-                $order,
-                $this->_scopeConfig->isSetFlag(
-                    self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID,
-                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-                    $order->getStoreId()
-                )
-            );
-            /* Add document text and number */
-            $this->insertDocumentNumber($page, __('Invoice # ') . $invoice->getIncrementId());
-            /* Add table */
-            $this->_drawHeader($page);
-            /* Add body */
-            foreach ($invoice->getAllItems() as $item) {
-                if ($item->getOrderItem()->getParentItem()) {
-                    continue;
-                }
-                /* Draw item */
-                $this->_drawItem($item, $page, $order);
-                $page = end($pdf->pages);
-            }
-            /* Add totals */
-            $this->insertTotals($page, $invoice);
-            if ($invoice->getStoreId()) {
-                $this->appEmulation->stopEnvironmentEmulation();
-            }
-        }
-        $this->_afterGetPdf();
-        return $pdf;
-    }
-
-    /**
-     * Create new page and assign to PDF object
-     *
-     * @param  array $settings
-     * @return \Zend_Pdf_Page
-     */
-    public function newPage(array $settings = [])
-    {
-        /* Add new table head */
-        $page = $this->_getPdf()->newPage(\Zend_Pdf_Page::SIZE_A4);
-        $this->_getPdf()->pages[] = $page;
-        $this->y = 800;
-        if (!empty($settings['table_header'])) {
-            $this->_drawHeader($page);
-        }
-        return $page;
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/dejavu-sans/DejaVuSansCondensed.ttf')
+        );
+        $object->setFont($font, $size);
+        return $font;
     }
 }
