a4f662916e9a144e35b1af7067b3f6f41a36d81f
Christian Kramer <ck.daill@gmail.com>
2021-02-19 16:42:23 +0100

    class still gets not generated

--- Sales/Model/Order/Pdf/CustomInvoice.php
+++ Sales/Model/Order/Pdf/CustomInvoice.php
@@ -13,32 +13,83 @@ use \Magento\Sales\Model\Order\Pdf\Invoice;
  */
 class CustomInvoice extends Invoice
 {
-    protected function insertTotals($page, $source)
+        /**
+     * Return PDF document
+     *
+     * @param array|Collection $invoices
+     * @return \Zend_Pdf
+     */
+    public function getPdf($invoices = [])
     {
-        $order = $source->getOrder();
-        $totals = $this->_getTotalsList();
-        $lineBlock = ['lines' => [], 'height' => 15];
-        foreach ($totals as $total) {
-            $total->setOrder($order)->setSource($source);
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
 
-            if ($total->canDisplay()) {
-                $total->setFontSize(10);
-                foreach ($total->getTotalsForDisplay() as $totalData) {
-                    $lineBlock['lines'][] = [
-                        [
-                            'text' => $totalData['label'],
-                            'feed' => 475,
-                            'align' => 'right',
-                            'font_size' => $totalData['font_size'],
-                            'font' => 'bold',
-                        ],
-                    ];
+        $pdf = new \Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new \Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                $this->appEmulation->startEnvironmentEmulation(
+                    $invoice->getStoreId(),
+                    \Magento\Framework\App\Area::AREA_FRONTEND,
+                    true
+                );
+                $this->_storeManager->setCurrentStore($invoice->getStoreId());
+            }
+            $page = $this->newPage();
+            $order = $invoice->getOrder();
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                $this->_scopeConfig->isSetFlag(
+                    self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID,
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $order->getStoreId()
+                )
+            );
+            /* Add document text and number */
+            $this->insertDocumentNumber($page, __('Invoice # ') . $invoice->getIncrementId());
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+
+            $this->insertTaxSentence($page);
+
+            foreach ($invoice->getAllItems() as $item) {
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
                 }
-            } 
-        }   
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                $this->appEmulation->stopEnvironmentEmulation();
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }
 
-        $this->y -= 20;
-        $page = $this->drawLineBlocks($page, [$lineBlock]);
-        return $page;       
+        /**
+     * Insert title and number for concrete document type
+     *
+     * @param  \Zend_Pdf_Page $page
+     * @param  string $text
+     * @return void
+     */
+    public function insertTaxSentence(\Zend_Pdf_Page $page)
+    {
+        $this->_setFontRegular($page, 10);
+        $page->drawText(__('Als Kleinunternehmer im Sinne von § 19 Abs. 1 UStG wird Umsatzsteuer nicht berechnet.'), 35, $this->y, 'UTF-8');
     }
 }
