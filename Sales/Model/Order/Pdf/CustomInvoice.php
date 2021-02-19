0b8f5c03e982d299a75698541e126ee0670cd996
Christian Kramer <ck.daill@gmail.com>
2021-02-19 21:00:35 +0100

    class still gets not generated

--- Sales/Model/Order/Pdf/CustomInvoice.php
+++ Sales/Model/Order/Pdf/CustomInvoice.php
@@ -69,6 +69,7 @@ class CustomInvoice extends Invoice
             $inlineTranslation,
             $addressRenderer,
             $storeManager,
+            $appEmulation,
             $data
         );
         $this->appEmulation = $appEmulation;
