e00d0f4b5e57082d928c8c2589ed8db4e3e5d494
Christian Kramer <ck.daill@gmail.com>
2021-02-19 15:13:34 +0100

    class still gets not generated

---
+++ Sales/Model/Order/Pdf/CustomInvoice.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Schraubermarkt\Sales\Model\Order\Pdf;
+
+use \Magento\Sales\Model\Order\Pdf\Invoice;
+
+/**
+ * Sales Order Invoice PDF model
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class CustomInvoice extends Invoice
+{
+
+}
