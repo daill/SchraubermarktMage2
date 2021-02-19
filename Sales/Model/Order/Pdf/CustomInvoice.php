0348fb784e3b71c319616d06433509b23634521a
Christian Kramer <ck.daill@gmail.com>
2021-02-19 16:26:42 +0100

    class still gets not generated

--- Sales/Model/Order/Pdf/CustomInvoice.php
+++ Sales/Model/Order/Pdf/CustomInvoice.php
@@ -13,5 +13,32 @@ use \Magento\Sales\Model\Order\Pdf\Invoice;
  */
 class CustomInvoice extends Invoice
 {
+    protected function insertTotals($page, $source)
+    {
+        $order = $source->getOrder();
+        $totals = $this->_getTotalsList();
+        $lineBlock = ['lines' => [], 'height' => 15];
+        foreach ($totals as $total) {
+            $total->setOrder($order)->setSource($source);
 
+            if ($total->canDisplay()) {
+                $total->setFontSize(10);
+                foreach ($total->getTotalsForDisplay() as $totalData) {
+                    $lineBlock['lines'][] = [
+                        [
+                            'text' => $totalData['label'],
+                            'feed' => 475,
+                            'align' => 'right',
+                            'font_size' => $totalData['font_size'],
+                            'font' => 'bold',
+                        ],
+                    ];
+                }
+            } 
+        }   
+
+        $this->y -= 20;
+        $page = $this->drawLineBlocks($page, [$lineBlock]);
+        return $page;       
+    }
 }
