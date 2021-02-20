c711b9aeb8ed7c539f816945814411f2a99b7d0a
Christian Kramer <ck.daill@gmail.com>
2021-02-20 10:32:28 +0100

    class still gets not generated

--- Sales/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Amazon_Payment/template/checkout-button.html': 
-              'Schraubermarkt_Sales/template/checkout-button.html'
-        }
-    }
-}
