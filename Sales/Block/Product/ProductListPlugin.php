cef0d5d7d7e9305813dbdcc9958874043c7485df
Christian Kramer <ck.daill@gmail.com>
2021-02-28 11:48:08 +0100

    add custom text to every price

---
+++ Sales/Block/Product/ProductListPlugin.php
@@ -0,0 +1,19 @@
+namespace Schraubermarkt\Sales\Block\Product;
+
+class ProductListPlugin {
+
+    /**
+     * @param Product $product
+     * @return string
+     */
+    public function afterGetProductPrice($subject, $result, $product)
+    {
+        $logger = \Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
+        $logger->info($result);
+
+        $subscriptionTextHTML = '<div class="custom_text"><span>It will display</span></div>';
+
+        return $result.$subscriptionTextHTML;
+    }
+
+}
