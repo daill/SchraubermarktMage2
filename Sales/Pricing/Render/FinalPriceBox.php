d96058511bd1d52a41d2fcda43c335afb58e5381
Christian Kramer <ck.daill@gmail.com>
2021-02-28 15:26:31 +0100

    changed custom text method

---
+++ Sales/Pricing/Render/FinalPriceBox.php
@@ -0,0 +1,30 @@
+<?php
+namespace PME\Hideprice\Pricing\Render;
+
+use Magento\Catalog\Pricing\Price;
+use Magento\Framework\Pricing\Render;
+use Magento\Framework\Pricing\Render\PriceBox as BasePriceBox;
+use Magento\Msrp\Pricing\Price\MsrpPrice;
+
+
+class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
+{
+    protected function wrapResult($html)
+    {
+     $result = '';
+     $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+     $httpContext=$objectManager->get('Magento\Framework\App\Http\Context');
+     $isLoggedIn = $httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+
+     if($isLoggedIn){
+       $result = '<div class="price-box ' . $this->getData('css_classes') . '" ' .
+        'data-role="priceBox" ' .
+        'data-product-id="' . $this->getSaleableItem()->getId() . '"' .
+        '>' . $html . '</div>';
+     }else{
+        $result = "<p>For any Quaries <a href='http://127.0.0.1/magento/contact/'>Contact Us </a></p>";
+     }
+    return $result;
+    }
+
+}
