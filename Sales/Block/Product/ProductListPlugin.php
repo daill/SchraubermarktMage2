a176edda787f7f136ff265530ca83c70d61171c5
Christian Kramer <ck.daill@gmail.com>
2021-02-28 12:06:24 +0100

    add custom text to every price

--- Sales/Block/Product/ProductListPlugin.php
+++ Sales/Block/Product/ProductListPlugin.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Schraubermarkt\Sales\Block\Product;
 
 class ProductListPlugin {
