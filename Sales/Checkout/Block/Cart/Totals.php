b9fb54e45a745cee4875f0883bc1519c1d767a77
Christian Kramer <ck.daill@gmail.com>
2021-02-28 20:34:38 +0100

    changed custom text method

--- Sales/Checkout/Block/Cart/Totals.php
+++ Sales/Checkout/Block/Cart/Totals.php
@@ -24,6 +24,7 @@ class Totals extends \Magento\Checkout\Block\Cart\Totals
             }
             $html .= $this->renderTotal($total, $area, $colspan);
         }
+        $html .= "<span>Some Text</span>"
         return $html;
     }
 
