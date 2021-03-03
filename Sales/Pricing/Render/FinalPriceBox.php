be39dd5957f2d65ba11e8a2dd70adff10832d1ad
Christian Kramer <ck.daill@gmail.com>
2021-03-03 20:02:00 +0100

    changed custom text method

--- Sales/Pricing/Render/FinalPriceBox.php
+++ Sales/Pricing/Render/FinalPriceBox.php
@@ -20,7 +20,9 @@ class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
         'data-role="priceBox" ' .
         'data-product-id="' . $this->getSaleableItem()->getId() . '"' .
         '>' . $html . '</div>
-        <span>Endpreis <a href="./versandkosten">zzgl. Versandkosten</a>, keine Ausweisung der Mehrwertsteuer gemäß § 19 UStG</span>';
+        <span>Endpreis <a href="/versandkosten">zzgl. Versandkosten</a>, keine Ausweisung der Mehrwertsteuer gemäß § 19 UStG</span>' .
+        '<span>Lieferzeit 1-3 Tage</span>';
+
     return $result;
     }
 
