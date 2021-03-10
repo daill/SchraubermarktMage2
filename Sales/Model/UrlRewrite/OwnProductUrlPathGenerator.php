0ab372df8cc5aa8c316bacffd9e566edddec1309
Christian Kramer <ck.daill@gmail.com>
2021-03-10 20:17:46 +0100

    changed custom text method

---
+++ Sales/Model/UrlRewrite/OwnProductUrlPathGenerator.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogUrlRewrite\Model;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Model product url path generator
+ */
+class OwnProductUrlPathGenerator extends /Magento/CatalogUrlRewrite/Model/ProductUrlPathGenerator {
+    /**
+     * Prepare url key for product
+     *
+     * @param Product $product
+     * @return string
+     */
+    protected function prepareProductUrlKey(Product $product)
+    {
+        $urlKey = (string)$product->getUrlKey();
+        $urlKey = trim(strtolower($urlKey));
+
+        return $product->formatUrlKey($urlKey ?: $product->getName() . "-" . $product->getSku());
+    }
+}
