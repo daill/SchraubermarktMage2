d434152b457604fb6cd0145ebc8d2c65a9e02ca0
Christian Kramer <ck.daill@gmail.com>
2021-03-15 14:01:39 +0100

    changed custom text method

--- Sales/view/frontend/requirejs-config.js
+++ Sales/view/frontend/requirejs-config.js
@@ -1,4 +1,9 @@
 var config = {
+	mixins: {
+            'Magento_Checkout/js/view/summary/abstract-total': {
+                'Schraubermarkt_Sales/js/abstract-total-mixin': true
+            }
+        },
     map: {
         '*': {
             'Amazon_Payment/template/checkout-button.html': 
