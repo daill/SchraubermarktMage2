8beb2bdb478f19745ff4f65acb5170d48e79f823
Christian Kramer <ck.daill@gmail.com>
2021-03-20 13:41:43 +0100

    changed custom text method

---
+++ Sales/view/frontend/web/js/checkout-details.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent'
+
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'QaisarSatti_HelloWorld/checkout/shipping/additional-block'
+        }
+    });
+});
