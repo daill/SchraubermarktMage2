34cfc62206ae8334e1904840b6e92a1dc8281882
Christian Kramer <ck.daill@gmail.com>
2021-04-10 10:33:37 +0200

    added new column to order overview

--- Sales/Block/Adminhtml/Sales/Sales/Grid.php
+++ Sales/Block/Adminhtml/Sales/Sales/Grid.php
@@ -197,30 +197,14 @@ class Grid extends \Magento\Reports\Block\Adminhtml\Grid\AbstractGrid
                 'column_css_class' => 'col-profit'
             ]
         );
-
-        $this->addColumn(
-            'total_cost_amount',
-            [
-                'header' => __('Gewinn'),
-                'type' => 'currency',
-                'currency_code' => $currencyCode,
-                'index' => 'total_cost_amount',
-                'total' => 'sum',
-                'sortable' => false,
-                'renderer' => Currency::class,
-                'rate' => $rate,
-                'header_css_class' => 'col-cost',
-                'column_css_class' => 'col-cost'
-            ]
-        );
-
+        
         $this->addColumn(
             'total_invoiced_amount',
             [
                 'header' => __('Invoiced'),
                 'type' => 'currency',
                 'currency_code' => $currencyCode,
-                'index' => 'total_invoiced_amount',
+                'index' => 'total_cost_amount',
                 'total' => 'sum',
                 'sortable' => false,
                 'renderer' => Currency::class,
