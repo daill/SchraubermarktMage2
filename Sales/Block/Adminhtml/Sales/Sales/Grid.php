7908d9587ce28250d0cbe46e449ab0a5b39cbc85
Christian Kramer <ck.daill@gmail.com>
2021-04-17 15:50:57 +0200

    added new column to order overview

--- Sales/Block/Adminhtml/Sales/Sales/Grid.php
+++ Sales/Block/Adminhtml/Sales/Sales/Grid.php
@@ -208,6 +208,7 @@ class Grid extends \Magento\Reports\Block\Adminhtml\Grid\AbstractGrid
                 'total' => 'sum',
                 'sortable' => false,
                 'renderer' => Currency::class,
+                'visibility_filter' => ['show_actual_columns'],
                 'rate' => $rate,
                 'header_css_class' => 'col-cost',
                 'column_css_class' => 'col-cost'
