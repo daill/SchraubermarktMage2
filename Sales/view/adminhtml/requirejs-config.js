364a5d5518b8d625edc6973d212b2dca40502780
Christian Kramer <ck.daill@gmail.com>
2023-01-18 20:48:21 +0100

    fixed rules.js

--- Sales/view/adminhtml/requirejs-config.js
+++ Sales/view/adminhtml/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
-    map: {
-        '*': {
-        'Magento_Ui/js/lib/validation/rules':'Schraubermarkt_Sales/js/lib/validation/rules'
-        }
+    mixins: {
+        'Magento_Ui/js/lib/validation/rules': {
+            'Schraubermarkt_Sales/js/lib/validation/rules': true
+        }, 
     }
 }
