ca8335db8252968547a4ff685db0a6b0364aec2a
Christian Kramer <ck.daill@gmail.com>
2023-01-19 21:16:46 +0100

    fixed rules.js

--- Sales/view/base/requirejs-config.js
+++ Sales/view/base/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map : {
         '*': 
         {
-            'Magento_Ui/js/lib/validation/validator':'Schraubermarkt_Sales/js/lib/validation/validator',
+           
         }
     }
 };
