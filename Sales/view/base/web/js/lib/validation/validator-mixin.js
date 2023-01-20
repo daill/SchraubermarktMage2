231c75859d52153ddecfd42ae73050e0077e0387
Christian Kramer <ck.daill@gmail.com>
2023-01-20 21:05:32 +0100

    fixed rules.js

---
+++ Sales/view/base/web/js/lib/validation/validator-mixin.js
@@ -0,0 +1,22 @@
+define([
+    'jquery',
+    '../Magento_Ui/js/lib/validation/utils.js'
+
+], function($) {
+    'use strict';
+  
+    return function(targetWidget) {
+      $.validator.addRule(
+        'validate-zero-or-greater',
+        function (value) {
+            if (typeof value == 'string' && value.indexOf(",") > -1) {
+                value = value.replace(/,/g, '.');
+            }
+            return utils.isEmptyNoTrim(value) || !isNaN(utils.parseNumber(value))
+                && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
+        },
+        $.mage.__('Please enter a number 0 or greater, without comma in this field.')
+      )
+      return targetWidget;
+    }
+  });
