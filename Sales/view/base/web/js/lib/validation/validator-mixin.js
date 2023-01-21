012e20ceeec2220e5f3d27b14a679d5b986a2cb8
Christian Kramer <ck.daill@gmail.com>
2023-01-21 13:53:40 +0100

    fixed rules.js

--- Sales/view/base/web/js/lib/validation/validator-mixin.js
+++ Sales/view/base/web/js/lib/validation/validator-mixin.js
@@ -14,7 +14,33 @@ define([
                     && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
             },
             $.mage.__('Please enter a number 0 or greater, without comma in this field.')
-      )
+        )
+
+        validator.addRule(
+            'validate-not-negative-number',
+            function (value) {
+                if (typeof value == 'string' && value.indexOf(",") > -1) {
+                    value = value.replace(/,/g, '.');
+                }
+                return $.mage.isEmptyNoTrim(value) || $.isNumeric(value)
+                    && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
+            },
+            $.mage.__('Please enter a number 0 or greater, without comma in this field.')
+        )
+
+        validator.addRule(
+            'validate-greater-than-zero',
+            function (value) {
+                if (typeof value == 'string' && value.indexOf(",") > -1) {
+                    value = value.replace(/,/g, '.');
+                }
+                return $.mage.isEmptyNoTrim(value) || $.isNumeric(value)
+                    && value > 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
+            },
+            $.mage.__('Please enter a number greater than 0, without comma in this field.')
+        )
+
+
       return validator;
     }
   });
