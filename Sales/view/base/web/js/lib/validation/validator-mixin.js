3fe673cab7271b67d4d742c048dca8fe5d7dfcf0
Christian Kramer <ck.daill@gmail.com>
2023-01-21 13:39:48 +0100

    fixed rules.js

--- Sales/view/base/web/js/lib/validation/validator-mixin.js
+++ Sales/view/base/web/js/lib/validation/validator-mixin.js
@@ -10,7 +10,7 @@ define([
                 if (typeof value == 'string' && value.indexOf(",") > -1) {
                     value = value.replace(/,/g, '.');
                 }
-                return $.mage.isEmptyNoTrim(value) || !$.mage.isNaN($.mage.parseNumber(value))
+                return $.mage.isEmptyNoTrim(value) || $.isNumeric(value)
                     && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
             },
             $.mage.__('Please enter a number 0 or greater, without comma in this field.')
