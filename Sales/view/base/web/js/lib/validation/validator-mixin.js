f46b7220077ecef438821e2a12dbf9177546b1bc
Christian Kramer <ck.daill@gmail.com>
2023-01-21 13:35:44 +0100

    fixed rules.js

--- Sales/view/base/web/js/lib/validation/validator-mixin.js
+++ Sales/view/base/web/js/lib/validation/validator-mixin.js
@@ -3,18 +3,18 @@ define([
 ], function($) {
     'use strict';
   
-    return function(targetWidget) {
-      $.validator.addRule(
-        'validate-zero-or-greater',
-        function (value) {
-            if (typeof value == 'string' && value.indexOf(",") > -1) {
-                value = value.replace(/,/g, '.');
-            }
-            return $.mage.isEmptyNoTrim(value) || !$.mage.isNaN(utils.parseNumber(value))
-                && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
-        },
-        $.mage.__('Please enter a number 0 or greater, without comma in this field.')
+    return function (validator) {
+        validator.addRule(
+            'validate-zero-or-greater',
+            function (value) {
+                if (typeof value == 'string' && value.indexOf(",") > -1) {
+                    value = value.replace(/,/g, '.');
+                }
+                return $.mage.isEmptyNoTrim(value) || !$.mage.isNaN(utils.parseNumber(value))
+                    && value >= 0 && (/^\s*-?\d+([,.]\d+)*\s*%?\s*$/).test(value);
+            },
+            $.mage.__('Please enter a number 0 or greater, without comma in this field.')
       )
-      return targetWidget;
+      return validator;
     }
   });
